e301205df75e3718600cab4424ea49bac551556c
scorscor <1006792421@qq.com>
2020-01-13 19:25:53 +0800

    11

--- js/api.js
+++ js/api.js
@@ -1,15 +1,35 @@
-function findById(params) { //面签页面--根据apid查询已有信息
-    return service({
-        url: '/api/backend/approvalProcess/findById',
-        method: 'get',
-        params
-    })
-}
-
-function faceSignSave(params) { //面签页面--保存提交
+/**
+ * 获取用户
+ * @param params
+ */
+function getAllUsers(params) { //面签页面--保存提交
     return service({
         url: '/api/getUser',
         method: 'post',
         data: JSON.stringify(params)
     })
+}
+
+/**
+ * 获取中奖配置
+ * @param params
+ */
+function getAllTime(params) {
+    return service({
+        url: '/api/getConfig',
+        method: 'post',
+        data: JSON.stringify(params)
+    })
+}
+
+/**
+ * 保存中奖信息
+ */
+function saveAwardUsers(params) {
+    console.log(params)
+    // return service({
+    //     url: '/api/save',
+    //     method: 'post',
+    //     data: JSON.stringify(params)
+    // })
 }
--- js/myMock.js
+++ js/myMock.js
@@ -1,5 +1,20 @@
 Mock.mock(
-    'http://192.168.1.92/api/getUser', {'Users|30-60':[{
+    'http://192.168.1.92/api/getUser',
+    {
+        'Users|90-160':[{
         'id|+1': 1,
-    }]
-});
+        'index|+1': 0,
+        }],
+
+        }
+    );
+Mock.mock(
+    'http://192.168.1.92/api/getConfig', {
+        award:[
+            {name:'iphonex',num:1,title:'一等奖'},
+            {name:'iphone11',num:3,title:'二等奖'},
+            {name:'iphone9',num:10,title:'三等奖'},
+            {name:'iphone8',num:20,title:'四等奖'}
+            ]//奖品&&人数
+    });
+
